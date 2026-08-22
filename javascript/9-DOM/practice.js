85d08c6d631a1b3ca7a15ca866c4097062df5280
Not-Animesh <animeshkr2022@gmail.com>
2026-08-23 00:47:59 +0530

    DOM

---
+++ javascript/9-DOM/practice.js
@@ -0,0 +1,28 @@
+//DOM manipluation
+//html se element ko select krna
+// text badlana
+//  html badlna
+//  css badlana
+// attribute badlana
+// event listener lagana
+
+
+//what is the DOM ? how does it represent the html structure ?
+//DOM is a tree like structure which represents the html structure of the page. It is an object representation of the html document. It is created by the browser when the html document is loaded. It is a live representation of the html document. It can be manipulated using javascript.
+
+// name the types of nodes in dom tree 
+//There are 12 types of nodes in the DOM tree. They are:
+//1. Element node
+//2. Text node
+//3. Attribute node
+//4. Comment node
+//5. Document node
+//6. DocumentType node
+//7. DocumentFragment node
+//8. ProcessingInstruction node
+//9. CDATASection node
+//10. EntityReference node
+//11. Entity node
+//12. Notation node
+
+
---
+++ javascript/9-DOM/script.js
@@ -0,0 +1,73 @@
+//DOM manipluation
+//html se element ko select krna
+// text badlana
+//  html badlna
+//  css badlana
+// attribute badlana
+// event listener lagana
+
+// let h1 = document.getElementById("1");
+// console.log(h1);
+// console.dir(h1);
+
+// let h2 = document.getElementsByClassName("huy");
+// console.log(h2);
+// console.dir(h2);
+
+
+// let h3 = document.querySelector("h1");
+// console.log(h3);
+// console.dir(h3);
+
+
+
+// let h4 = document.querySelectorAll("h1");
+// console.log(h4);
+// console.dir(h4);
+
+// let h4 = document.querySelectorAll("h1")[0];
+// h4.textContent = "hey hye hey";
+// console.log(h4);
+// // console.dir(h4);
+
+
+// let h4 = document.querySelectorAll("h1");
+// h4[0].innerHTML = "<i>hey hye hey</i>";
+// console.log(h4);
+// console.dir(h4);
+
+// let a = document.querySelector("a");
+// a.setAttribute("href", "https://www.google.com");
+// a.setAttribute("target", "_blank");
+
+
+
+// let a = document.querySelector("a");
+// a.removeAttribute("href");
+
+
+//create element
+//append/prepend element kro jahai bhi element chahiye
+
+// let h1 = document.createElement("h1");
+// h1.textContent = "Hello, World!";
+// document.body.appendChild(h1);
+// console.log(h1);
+
+
+
+// let h1 = document.createElement("h1");
+// h1.textContent = "Hello, World!";
+
+// document.body.prependChild(h1);
+
+// document.querySelector("body").appendChild(h1);
+
+
+// console.log(h1);
+
+
+// let h1 =  document.querySelector("h1");
+// h1.style.color = "red";
+
+// h1.classList.add("huy");
